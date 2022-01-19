d453dd07bc6f78bc6313a5d5d58e67b94cb53050
Ivan Achenbach <ivan.achenb@gmail.com>
2022-01-19 09:07:38 -0300

    Script R solapa facturas emitidas reporte de Facturacion

--- DBA/Reportes BI/2021/Facturación/Script Solapa Facturas Emitidas.R
+++ DBA/Reportes BI/2021/Facturación/Script Solapa Facturas Emitidas.R
@@ -4,10 +4,23 @@ workdirectory_two <- "E:/Personales/Sistemas/Agustin/Reportes BI/2021/Facturació
 
 source("C:/Users/iachenbach/Desktop/Facoep - Scripts/DBA/Reportes BI/2021/Facturación/Script_Facturacion_Funciones.R")
 
-archivo_parametros <- GetArchivoParametros(path_one = WorkdirectoryOne,
-                                           path_two = WorkdirectoryTwo,
+archivo_parametros <- GetArchivoParametros(path_one = workdirectory_one,
+                                           path_two = workdirectory_two,
                                            file = "parametros_servidor.xlsx")
 
+tipo_comprobantes <- GetFile("tipo_comprobante.xlsx",
+                             path_one = workdirectory_one,
+                             path_two = workdirectory_two)
+
+
+tipo_comprobantes$Comprobante <- tipo_comprobantes$Tipo.Comprobante
+
+
+comprobantes_query <- GetListaINSQL(tipo_comprobantes)
+
+CentrosCostos  <- GetFile("centro_costo_comprobantes.xlsx",
+                                              path_one = workdirectory_one,
+                                              path_two = workdirectory_two)
 
 pw <- GetPassword()
 
@@ -23,33 +36,65 @@ con <- dbConnect(drv, dbname = "facoep",
                  password = pw)
 
 
-
-
-
-
-
-
 postgresqlpqExec(con, "SET client_encoding = 'windows-1252'")
 
 
-query <- "SELECT comprobanteccosto,
+comprobantes <- glue("SELECT comprobanteccosto,
                       CAST(os.clienteid AS TEXT) || ' - ' || CAST(os.clientenombre AS TEXT) as OS,
                       c.tipocomprobantecodigo,
                       c.comprobanteprefijo,
                       c.comprobantecodigo,
                       c.comprobantefechaemision,
                       c.comprobantetotalimporte,
-                      c.comprobantedetalle,
-                      os.obsocialescodigo
+                      CASE WHEN c.comprobantedetalle LIKE '%ANULADO%' THEN 'SI' ELSE 'NO' END AS Anulado, 
+                      os.clienteid
 
 
 
   FROM comprobantes c
    LEFT JOIN clientes os ON os.clienteid = c.comprobanteentidadcodigo
                                   
-  WHERE c.comprobantetipoentidad = 2 and "
+  WHERE c.comprobantetipoentidad = 2 and c.tipocomprobantecodigo IN ({comprobantes_query})")
 
 
+comprobantes <- dbGetQuery(con,comprobantes) 
 
-Bruto <- dbGetQuery(con,query) 
+comprobantes <- CleanTablaComprobantes(comprobantes)
 
+#Revisar esta funcion
+#comprobantes$comprobanteccosto <- ifelse(is.null(comprobantes$comprobanteccosto),5,comprobantes$comprobanteccosto)
+
+unique(comprobantes$comprobanteccosto)
+
+comprobantes <- left_join(comprobantes,CentrosCostos,by = c("comprobanteccosto" = "id.centro.costo"))
+
+comprobantes$comprobanteccosto <- NULL
+
+tipo_comprobantes <- data.frame("TipoComprobante" = tipo_comprobantes$Tipo.Comprobante,"Multiplicador"=tipo_comprobantes$Multiplicador)
+
+comprobantes <- left_join(comprobantes,tipo_comprobantes,by = c("tipocomprobantecodigo" = "TipoComprobante"))
+
+comprobantes$comprobantetotalimporte <- comprobantes$comprobantetotalimporte * comprobantes$Multiplicador
+
+comprobantes$Multiplicador <- NULL
+
+comprobantes$NombreMes <- months(as.Date(comprobantes$comprobantefechaemision))
+
+comprobantes$Mes <- month(as.Date(comprobantes$comprobantefechaemision))
+
+comprobantes$Anio <- year(as.Date(comprobantes$comprobantefechaemision))
+
+comprobantes$dia <- day(as.Date(comprobantes$comprobantefechaemision))
+
+comprobantes <- select(comprobantes,
+                       "Financiador" = os,
+                       "tipocomprobantecodigo" = tipocomprobantecodigo,
+                       "Fecha Emision" = comprobantefechaemision,
+                       "Centro de Costos" = Nombre,
+                       "Anio" = Anio,
+                       "importe"= comprobantetotalimporte,
+                       "Anulado" = anulado,
+                       "Mes" = Mes,
+                       "Dia" = dia,
+                       "Nombre del Mes" = NombreMes,
+                       "NroComprobante" = NroComprobante)
