3e30c2cd4419b311540995f67175c9af9d2ccab0
Ivan Achenbach <ivan.achenb@gmail.com>
2022-01-21 09:13:03 -0300

    Incorporacion de Centro de Costos y Subcentro a la solapa Usuarios del reporte de Auditoria Medica V.1.0.0

--- DBA/Reportes BI/2021/AuditoriaMedica/UsuariosAuditoriaMedica.R
+++ DBA/Reportes BI/2021/AuditoriaMedica/UsuariosAuditoriaMedica.R
@@ -1,7 +1,8 @@
-workdirectory_one <- "C:/Users/iachenbach/Desktop/FACOEP/DBA/Reportes BI/2021/AuditoriaMedica/V2"
-workdirectory_two <- "E:/Personales/Sistemas/Agustin/Reportes BI/2021/Cobranzas/Versi?n 7"
+workdirectory_one <- "C:/Users/iachenbach/Gobierno de la Ciudad de Buenos Aires/Pablo Alfredo Gadea - Tablero Facoep P BI/FACOEP/DBA/Reportes BI/2021/AuditoriaMedica"
+workdirectory_two <- "E:/Personales/Sistemas/Agustin/Reportes BI/2021/AuditoriaMedica/V2"
 
 source("C:/Users/iachenbach/Desktop/FACOEP/DBA/Reportes BI/2021/AuditoriaMedica/V2/Script_AuditoriaMedica_Funciones.r")
+
 
 archivo_parametros <- GetArchivoParametros(path_one = workdirectory_one, 
                                            path_two = workdirectory_two, 
@@ -30,13 +31,22 @@ medicos <- as.vector(medicos)
 medicos <- toString(sprintf("'%s'", medicos))
 
 
-CONSULTA <- glue("SELECT crgfchemision,crgauditoriamedicafecha,crgdetfechaalta,
-                                    crgauditoriamedicausuario, crgdetnumerocph,
-                                    c.crgnum, crgdetmotivodebcred, 
-                            crgdetmotivodescripcion, crgimpbrutooriginal, crgimpbruto, crgdetimportefacturar,
-                            (SELECT MAX(crghistorialfecha) FROM crghistorial WHERE crghistorial.crgnum = c.crgnum AND crghistorial.pprid = c.pprid AND crghistorialestado = 3 AND crghistorialcod >= 2 AND crghistorialfecha > '2017-01-01') AS auditoria
-                            FROM crg c
-                            LEFT JOIN crgdet cd ON c.crgnum = cd.crgnum and c.pprid = cd.pprid WHERE crgauditoriamedicausuario IN ({medicos})")
+CONSULTA <- glue("SELECT crgfchemision,
+                         crgauditoriamedicafecha,
+                         crgdetfechaalta,
+                         crgauditoriamedicausuario,
+                         crgdetnumerocph,
+                         c.crgnum,
+                         crgdetmotivodebcred, 
+                         crgdetmotivodescripcion, 
+                         crgimpbrutooriginal, 
+                         crgimpbruto, 
+                         crgdetimportefacturar,
+                         c.crgccostocodigo as CentroCosto,
+                         c.crgsccostocodigo as SubCentroCosto,
+                         (SELECT MAX(crghistorialfecha) FROM crghistorial WHERE crghistorial.crgnum = c.crgnum AND crghistorial.pprid = c.pprid AND crghistorialestado = 3 AND crghistorialcod >= 2 AND crghistorialfecha > '2017-01-01') AS auditoria
+                         FROM crg c
+                         LEFT JOIN crgdet cd ON c.crgnum = cd.crgnum and c.pprid = cd.pprid WHERE crgauditoriamedicausuario IN ({medicos})")
 
 
 CONSULTA <- dbGetQuery(con, CONSULTA)
@@ -46,15 +56,12 @@ CONSULTA <- filter(CONSULTA, !is.na(auditoria))
 
 CONSULTA$crgauditoriamedicausuario<- gsub(" ","",CONSULTA$crgauditoriamedicausuario)
   
-
 CONSULTA$crgdetfechaalta <- as.Date(CONSULTA$crgdetfechaalta)
-
 CONSULTA$auditoria <- as.Date(CONSULTA$auditoria)
 
 CONSULTA$crgauditoriamedicafecha <- fifelse(CONSULTA$crgauditoriamedicafecha == "0001-01-01", CONSULTA$auditoria, CONSULTA$crgauditoriamedicafecha)
-
 CONSULTA$crgauditoriamedicafecha <- fifelse(is.na(CONSULTA$crgauditoriamedicafecha), CONSULTA$auditoria, CONSULTA$crgauditoriamedicafecha)
-
+CONSULTA$crgauditoriamedicafecha <- fifelse(CONSULTA$crgauditoriamedicafecha < "2021-01-01", CONSULTA$auditoria, CONSULTA$crgauditoriamedicafecha)
 
 AuditoriaMedica <- select(CONSULTA, "FechaAuditoriaMedica" = crgauditoriamedicafecha,
                           "Auditor" = crgauditoriamedicausuario,
@@ -62,18 +69,35 @@ AuditoriaMedica <- select(CONSULTA, "FechaAuditoriaMedica" = crgauditoriamedicaf
                           "DPH" = crgdetnumerocph,
                           "Agrega" = crgdetmotivodebcred,
                           "AgregadoImporte" = crgdetimportefacturar,
-                          "ImporteFinal" = crgimpbruto)
+                          "ImporteFinal" = crgimpbruto,
+                          "CentroCosto" = centrocosto,
+                          "SubCentroCosto" = subcentrocosto)
 
-AuditoriaMedica <- aggregate(.~FechaAuditoriaMedica+Auditor+CRG+DPH+Agrega+ImporteFinal, AuditoriaMedica, sum)
+AuditoriaMedica <- aggregate(.~FechaAuditoriaMedica+Auditor+CRG+DPH+Agrega+ImporteFinal+CentroCosto+SubCentroCosto, AuditoriaMedica, sum)
 
+AuditoriaMedica$AgregadoImporte <- ifelse(AuditoriaMedica$Agrega == 42, AuditoriaMedica$AgregadoImporte, 0)
+AuditoriaMedica$Agrega <- NULL
+
+AuditoriaMedica <- aggregate(.~FechaAuditoriaMedica+Auditor+CRG+DPH+CentroCosto+SubCentroCosto+ImporteFinal, AuditoriaMedica, sum)
 
 AuditoriaMedica$CantidadDPH <- 1
 AuditoriaMedica$DPH <- NULL
 
+AuditoriaMedica <- aggregate(.~FechaAuditoriaMedica+Auditor+CRG+CentroCosto+SubCentroCosto+ImporteFinal, AuditoriaMedica, sum)
 
 AuditoriaMedica$ImporteOriginal <- AuditoriaMedica$ImporteFinal - AuditoriaMedica$AgregadoImporte
 
+AuditoriaMedica$CantidadCRG <- 1
+AuditoriaMedica$CRG <- NULL
+AuditoriaMedica <- aggregate(.~FechaAuditoriaMedica+Auditor+CentroCosto+SubCentroCosto, AuditoriaMedica, sum)
 
-
-
+AuditoriaMedica <- select(AuditoriaMedica,
+                          "Fecha" = FechaAuditoriaMedica,
+                          Auditor,
+                          CantidadCRG, CantidadDPH,
+                          ImporteOriginal,
+                          AgregadoImporte,
+                          ImporteFinal,
+                          CentroCosto,
+                          SubCentroCosto)
 
