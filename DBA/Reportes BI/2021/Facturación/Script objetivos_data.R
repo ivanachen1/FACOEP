8d7d18eb24302cd4a6e4f26f3c08bef68f457898
Ivan Achenbach <ivan.achenb@gmail.com>
2022-02-23 09:37:53 -0300

    SIF 2 Facturacion Terminado, Revisar numeros y corregir

--- DBA/Reportes BI/2021/Facturación/Script objetivos_data.R
+++ DBA/Reportes BI/2021/Facturación/Script objetivos_data.R
@@ -26,15 +26,40 @@ tipo_comprobantes <- GetFile("tipo_comprobante.xlsx",
                              path_one = workdirectory_one,
                              path_two = workdirectory_two)
 
+tipo_comprobantes <- select(tipo_comprobantes,
+                            "TipoComprobante" = Tipo.Comprobante,
+                            "SIF2" = Alimenta.a.SIF2)
 
-tipo_comprobantes$Comprobante <- tipo_comprobantes$Tipo.Comprobante
+Efectores <- GetFile("Efectores.xlsx",
+                             path_one = workdirectory_one,
+                             path_two = workdirectory_two)
 
+
+tipo_comprobantes$Comprobante <- tipo_comprobantes$TipoComprobante
+
+tipo_comprobantes <- filter(tipo_comprobantes,SIF2 == TRUE)
 
 comprobantes_query <- GetListaINSQL(tipo_comprobantes)
+
+print(comprobantes_query)
 
 CentrosCostos  <- GetFile("centro_costo_comprobantes.xlsx",
                           path_one = workdirectory_one,
                           path_two = workdirectory_two)
+
+CodigosOOSSDesestimar <- GetFile(file_name = "Codigos Obra Social a Desestimar.xlsx",
+                                 path_one = workdirectory_one,
+                                 path_two = workdirectory_two)
+
+ComprobantesDesestimar <- GetFile(file_name = "comprobantes a desestimar.xlsx",
+                                 path_one = workdirectory_one,
+                                 path_two = workdirectory_two)
+
+
+ComprobantesDesestimar$Comprobante <- ComprobantesDesestimar$Listado.Comprobantes.a.desestimar
+
+ComprobantesDesestimar<- GetListaINSQL(ComprobantesDesestimar)
+
 
 pw <- GetPassword()
 
@@ -53,7 +78,8 @@ con <- dbConnect(drv, dbname = "facoep",
 postgresqlpqExec(con, "SET client_encoding = 'windows-1252'")
 
 
-BrutoQuery <- "SELECT pprnombre,
+BrutoQuery <- glue("SELECT pprnombre,
+                      CASE WHEN (pprnombre LIKE'%CES%') THEN 'CESAC' WHEN (pprnombre LIKE'%CEM%') THEN 'CESAC' ELSE pprnombre END AS pprnombre2,
                       obsocialessigla as os,
                       c.tipocomprobantecodigo,
                       c.comprobanteprefijo,
@@ -75,11 +101,25 @@ BrutoQuery <- "SELECT pprnombre,
                       LEFT JOIN obrassociales os ON os.obsocialescodigo = crg.comprobantecrgos
                       LEFT JOIN proveedorprestador pp on pp.pprid = crg.comprobantepprid
                                   
-                      WHERE c.comprobantetipoentidad = 2 and comprobantepprid > 0"
+                      WHERE c.comprobantetipoentidad = 2 and comprobantepprid > 0 and c.tipocomprobantecodigo IN({comprobantes_query}) AND c.comprobantecodigo NOT IN ({ComprobantesDesestimar})")
 
 
 Bruto <- dbGetQuery(con,BrutoQuery)
 
 Bruto <- CleanTablaComprobantes(Bruto)
 
-Bruto$obsocialescodigo <- as.
+Bruto$comprobantefechaemision <- as.Date(Bruto$comprobantefechaemision)
+
+Bruto <- unique(Bruto)
+
+Bruto$AnioEmision <- year(Bruto$comprobantefechaemision)
+
+Bruto <- left_join(Bruto,Efectores, by = c("pprnombre" = "sif"))
+
+Bruto <- aggregate(Bruto$comprobantecrgimporteneto,by = list(Bruto$Efector,Bruto$Anio),FUN = sum)
+
+colnames(Bruto) <- c("Efector","Anio","Total Facturado")
+
+Bruto$Fk <- paste(Bruto$Anio,Bruto$Efector,sep = "-")
+
+
