231b4e39514485ce3758b2df70c53c9841a06b05
Ivan Achenbach <ivan.achenb@gmail.com>
2022-03-30 08:28:18 -0300

    Join con tabla comprobantecrg para obtener la apertura de los recibos

--- DBA/Reportes BI/2021/Cobranza por Prestaciones/Script Cobranza por Prestación.R
+++ DBA/Reportes BI/2021/Cobranza por Prestaciones/Script Cobranza por Prestación.R
@@ -49,7 +49,8 @@ query <- glue("SELECT
               CAST(os.clienteid AS TEXT) || ' - ' || CAST(clientenombre AS TEXT) as OS,
               CONCAT(c.tipocomprobantecodigo,'-',c.comprobanteprefijo,'-',c.comprobantecodigo) as recibo,
               c.comprobantefechaemision as emision,
-              cd.comprobantecrgnro,
+              cc.comprobantecrgnro as comprobantecrgnrocrg,
+              cd.comprobantecrgnro as comprobantecrgdetnrocrg,
               comprobantecrgdetpractica as Prestacion,
               comprobantecrgdetimportefactur as ImportePrestacion,
               c.comprobanteccosto as CentroCosto
@@ -59,17 +60,36 @@ query <- glue("SELECT
               comprobantes c
               
               LEFT JOIN 
-              comprobantecrgdet cd ON cd.comprobantetipoentidad = c.comprobantetipoentidad and
-              cd.comprobanteentidadcodigo = c.comprobanteentidadcodigo and
-              cd.tipocomprobantecodigo = c.tipocomprobantecodigo and
-              cd.comprobanteprefijo = c.comprobanteprefijo and
-              cd.comprobantecodigo = c.comprobantecodigo
+              (SELECT tipocomprobantecodigo,
+                      comprobanteprefijo,
+                      comprobantecodigo,
+                      comprobantepprid,
+                      comprobantecrgnro
+               FROM comprobantecrg) as cc
+               
+               ON 
+               
+               c.tipocomprobantecodigo = cc.tipocomprobantecodigo and
+               c.comprobanteprefijo = cc.comprobanteprefijo and
+               c.comprobantecodigo = cc.comprobantecodigo
+              
+              LEFT JOIN 
+              comprobantecrgdet cd 
+              
+              ON 
+
+              cd.tipocomprobantecodigo = cc.tipocomprobantecodigo and
+              cd.comprobanteprefijo = cc.comprobanteprefijo and
+              cd.comprobantecodigo = cc.comprobantecodigo and
+              cd.comprobantecrgnro = cc.comprobantecrgnro and
+              cd.comprobantepprid = cc.comprobantepprid
+              
               
               LEFT JOIN 
               clientes os ON os.clienteid = c.comprobanteentidadcodigo
               
               LEFT JOIN 
-              proveedorprestador pp ON pp.pprid = cd.comprobantepprid
+              proveedorprestador pp ON pp.pprid = cc.comprobantepprid
               
               WHERE c.tipocomprobantecodigo IN ('RECX2') and c.comprobantefechaemision > '2017-01-01'")
 
@@ -78,10 +98,10 @@ data <- dbGetQuery(con,query)
 
 data$recibo <- gsub(" ","",data$recibo)
 
-data$PoseeCrg <- ifelse(is.na(data$comprobantecrgnro),"No","Si")
-data$TipoApertura <- ifelse(data$PoseeCrg == "No",
+
+data$TipoApertura <- ifelse(is.na(data$comprobantecrgnrocrg) & is.na(data$comprobantecrgnrocrg),
                             "Sin Apertura",
-                            ifelse(!is.na(data$comprobantecrgnro) && is.na(data$prestacion),
+                            ifelse(!is.na(data$comprobantecrgnrocrg) & is.na(data$comprobantecrgdetnrocrg),
                                    "Apertura Cabecera",
                                    "Apertura Detalle"))
 
@@ -94,9 +114,6 @@ RecibosConSinApertura <- unique(select(data,"Recibo" = recibo,
 RecibosConSinApertura$Efector <- ifelse(is.na(RecibosConSinApertura$Efector),"Sin Asignar",RecibosConSinApertura$Efector)
 
 RecibosConSinApertura$ObraSocial <- ifelse(is.na(RecibosConSinApertura$ObraSocial),"Sin Asignar",RecibosConSinApertura$ObraSocial)
-
-ft <- data.frame(table(RecibosConSinApertura$TipoApertura))
-
 
 data$cantidad <- 1
 
@@ -115,7 +132,7 @@ data$efector <- ifelse(is.na(data$efector),"Sin Asignar SIF",data$efector)
 
 data$os <- ifelse(is.na(data$os),"Sin Asignar SIF",data$os)
 
-data <- aggregate(.~efector+os+recibo+emision+comprobantecrgnro+prestacion+importeprestacion+centrocosto+PoseeCrg+TipoApertura,
+data <- aggregate(.~efector+os+recibo+emision+comprobantecrgnro+prestacion+importeprestacion+centrocosto+TipoApertura,
           data, sum)
 
 CentroCostos <- dbGetQuery(conn = con,"SELECT * FROM centrocostos")
@@ -123,5 +140,5 @@ CentroCostos <- dbGetQuery(conn = con,"SELECT * FROM centrocostos")
 
 lapply(dbListConnections(drv = dbDriver("PostgreSQL")), function(x) {dbDisconnect(conn = x)})
 
-
+ft <- data.frame(table(RecibosConSinApertura$TipoApertura))
 
