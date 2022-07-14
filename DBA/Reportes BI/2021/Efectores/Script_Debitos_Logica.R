d993ad1294217b12d46901486a44919f2cda6de8
ivanachen1 <ivan.achenb@gmail.com>
2022-07-14 08:53:12 -0300

    Logica Refactura y Auditoria Medica

--- DBA/Reportes BI/2021/Efectores/Script_Debitos_Logica.R
+++ DBA/Reportes BI/2021/Efectores/Script_Debitos_Logica.R
@@ -1,37 +1,30 @@
-workdirectory_one <- "C:/Users/iachenbach/Gobierno de la Ciudad de Buenos Aires/Pablo Alfredo Gadea - Tablero Facoep P BI/FACOEP/DBA/Reportes BI/2021/detalle debitos"
-workdirectory_two <- "E:/Personales/Sistemas/Agustin/Reportes BI/2021/Efectores/version2"
+workdirectory <- "C:/Users/Usuario/Desktop/otros/FACOEP/DBA/Reportes BI/2021/detalle debitos"
+#workdirectory <- "E:/Personales/Sistemas/Agustin/Reportes BI/2021/detalle debitos"
 Archivo <-"Script_debitos_Funciones.R"
-#source("C:/Users/iachenbach/Desktop/Facoep - Scripts/DBA/Reportes BI/2021/Facturación/Script_Facturacion_Funciones.R")
+#source("C:/Users/iachenbach/Desktop/Facoep - Scripts/DBA/Reportes BI/2021/FacturaciÃ³n/Script_Facturacion_Funciones.R")
 
 
-GetFileAux <- function(workdirectory_one,workdirectory_two,Archivo){
-  intento <- try(source(paste(workdirectory_one,Archivo,sep = "/")),silent = TRUE)
-  if (class(intento) == "try-error"){
-    return(source(paste(workdirectory_two,Archivo,sep = "/")))} else {return(source(paste(workdirectory_one,Archivo,sep = "/")))}
-}
+source(paste(workdirectory,Archivo,sep = "/"))
 
 
 
-GetFileAux(workdirectory_one = workdirectory_one,
-           workdirectory_two = workdirectory_two,
-           Archivo = Archivo)
-
-
-
-archivo_parametros <- GetArchivoParametros(path_one = workdirectory_one,
-                                           path_two = workdirectory_two,
+archivo_parametros <- GetArchivoParametros(path_one = workdirectory,
+                                           path_two = workdirectory,
                                            file = "parametros_servidor.xlsx")
 
 
-pw <- GetPassword()
+pw <- GetParameter(x = archivo_parametros,
+                   parameter = "password")
 
 drv <- dbDriver("PostgreSQL")
 
-user <- GetUser()
+user <- GetParameter(x = archivo_parametros,parameter = "user")
 
-host <- GetHost()
+host <- GetParameter(x = archivo_parametros,parameter = "host")
 
-con <- dbConnect(drv, dbname = "facoep", 
+database <- GetParameter(x = archivo_parametros,parameter = "database")
+
+con <- dbConnect(drv, dbname = database, 
                  host = host,
                  port = 5432,
                  user = user,
@@ -39,44 +32,75 @@ con <- dbConnect(drv, dbname = "facoep",
 
 
 SelectQuery <- paste("SELECT",
-                 "comprobantefechaemision,",
-                 "CONCAT(nota.tipocomprobantecodigo,'-',nota.comprobanteprefijo,'-',nota.comprobantecodigo) AS notadb,",
-                 "CONCAT(nota.comprobanteentidadcodigo,'-',os.obsocialessigla) AS OOSS,",
-                 "hosp.pprnombre as Efector,",
-                 "crgs.comprobantecrgnro as CRG,",
-                 "mot.motivodebitodescripcion as TipoDebito,",
-                 "cat.motivodebitocategorianombre as Categoria,",
-                 "dets.comprobantecrgdetmotivodescrip as Observaciones,",
-                 "dets.ComprobanteCRGDetImporteDebita as Rechazado,",
-                 "dets.ComprobanteCRGDetImporteAcredi as Aceptado,",
-                 "dets.comprobantecrgdetpractica as codigo",
-                 "FROM COMPROBANTES nota",
-                 "LEFT JOIN COMPROBANTECRG crgs ON crgs.empcod = nota.empcod AND", 
-                 "crgs.sucursalcodigo = nota.sucursalcodigo AND",
-                 "crgs.comprobantetipoentidad = nota.comprobantetipoentidad AND",
-                 "crgs.comprobanteentidadcodigo = nota.comprobanteentidadcodigo AND",
-                 "crgs.tipocomprobantecodigo = nota.tipocomprobantecodigo AND",
-                 "crgs.comprobanteprefijo = nota.comprobanteprefijo AND",
-                 "crgs.comprobantecodigo = nota.comprobantecodigo",
-                 "LEFT JOIN COMPROBANTECRGDET dets ON dets.empcod = crgs.empcod AND",
-                 "dets.sucursalcodigo = crgs.sucursalcodigo AND",
-                 "dets.comprobantetipoentidad = crgs.comprobantetipoentidad AND",
-                 "dets.comprobanteentidadcodigo = crgs.comprobanteentidadcodigo AND",
-                 "dets.tipocomprobantecodigo = crgs.tipocomprobantecodigo AND",
-                 "dets.comprobanteprefijo = crgs.comprobanteprefijo AND",
-                 "dets.comprobantecodigo = crgs.comprobantecodigo AND",
-                 "dets.comprobantepprid = crgs.comprobantepprid AND",
-                 "dets.comprobantecrgnro = crgs.comprobantecrgnro",
-                 "LEFT JOIN proveedorprestador hosp ON hosp.pprid = dets.comprobantepprid",
-                 "LEFT JOIN motivodebito mot ON mot.motivodebitoid = dets.comprobantecrgdetmotivodebcred",
-                 "LEFT JOIN motivodebitocategoria cat ON cat.motivodebitoid = dets.comprobantecrgdetmotivodebcred AND cat.motivodebitocategoriaid = dets.comprobantecrgdetmotdebcredcat",
-                 "LEFT JOIN obrassociales os ON os.obsocialesclienteid = nota.comprobanteentidadcodigo",
-                 "WHERE nota.tipocomprobantecodigo IN ('NOTADB')")
+                     "comprobantefechaemision,",
+                     "CONCAT(nota.tipocomprobantecodigo,'-',nota.comprobanteprefijo,'-',nota.comprobantecodigo) AS notadb,",
+                     "CONCAT(nota.comprobanteentidadcodigo,'-',os.obsocialessigla) AS OOSS,",
+                     "hosp.pprnombre as Efector,",
+                     "crgs.comprobantecrgnro as CRG,",
+                     "cg.crgauditoriamedicausuario as auditor,",
+                     "mot.motivodebitodescripcion as TipoDebito,",
+                     "cat.motivodebitocategorianombre as Categoria,",
+                     "dets.comprobantecrgdetmotivodescrip as Observaciones,",
+                     "dets.ComprobanteCRGDetImporteDebita as Rechazado,",
+                     "dets.ComprobanteCRGDetImporteAcredi as Aceptado,",
+                     "dets.comprobantecrgdetpractica as codigo,",
+                     "auditoria.tipocomprobantecodigo AS EnAuditoria",
+                     "FROM COMPROBANTES nota",
+                     "LEFT JOIN COMPROBANTECRG crgs ON crgs.empcod = nota.empcod AND", 
+                     "crgs.sucursalcodigo = nota.sucursalcodigo AND",
+                     "crgs.comprobantetipoentidad = nota.comprobantetipoentidad AND",
+                     "crgs.comprobanteentidadcodigo = nota.comprobanteentidadcodigo AND",
+                     "crgs.tipocomprobantecodigo = nota.tipocomprobantecodigo AND",
+                     "crgs.comprobanteprefijo = nota.comprobanteprefijo AND",
+                     "crgs.comprobantecodigo = nota.comprobantecodigo",
+                     "LEFT JOIN COMPROBANTECRGDET dets ON dets.empcod = crgs.empcod AND",
+                     "dets.sucursalcodigo = crgs.sucursalcodigo AND",
+                     "dets.comprobantetipoentidad = crgs.comprobantetipoentidad AND",
+                     "dets.comprobanteentidadcodigo = crgs.comprobanteentidadcodigo AND",
+                     "dets.tipocomprobantecodigo = crgs.tipocomprobantecodigo AND",
+                     "dets.comprobanteprefijo = crgs.comprobanteprefijo AND",
+                     "dets.comprobantecodigo = crgs.comprobantecodigo AND",
+                     "dets.comprobantepprid = crgs.comprobantepprid AND",
+                     "dets.comprobantecrgnro = crgs.comprobantecrgnro",
+                     "LEFT JOIN proveedorprestador hosp ON hosp.pprid = dets.comprobantepprid",
+                     "LEFT JOIN motivodebito mot ON mot.motivodebitoid = dets.comprobantecrgdetmotivodebcred",
+                     "LEFT JOIN motivodebitocategoria cat ON cat.motivodebitoid = dets.comprobantecrgdetmotivodebcred AND cat.motivodebitocategoriaid = dets.comprobantecrgdetmotdebcredcat",
+                     "LEFT JOIN obrassociales os ON os.obsocialesclienteid = nota.comprobanteentidadcodigo",
+                     "LEFT JOIN crg cg ON dets.comprobantepprid = cg.pprid AND dets.comprobantecrgnro = cg.crgnum",
+                     "LEFT JOIN auditoriascompartidascomproban auditoria ON",
+                     "nota.tipocomprobantecodigo = auditoria.tipocomprobantecodigo AND",
+                     "nota.comprobanteprefijo = auditoria.comprobanteprefijo AND",
+                     "nota.comprobantecodigo = auditoria.comprobantecodigo",
+                     "WHERE nota.tipocomprobantecodigo IN ('NOTADB')")
 
 
 MotivosDebitos <- dbGetQuery(con,SelectQuery)
 
-lapply(dbListConnections(drv = dbDriver("PostgreSQL")), function(x) {dbDisconnect(conn = x)})
+MotivosDebitos$EsAuditoriaCompartida <- ifelse(is.na(MotivosDebitos$enauditoria),
+                                               "NO","SI")
+
+FacturasQuery <- paste("SELECT",
+                     "CONCAT(nota.tipocomprobantecodigo,'-',nota.comprobanteprefijo,'-',nota.comprobantecodigo) AS notadb,",
+                     "CONCAT(asociados.comprobanteasoctipo,'-',asociados.comprobanteasocprefijo,'-',asociados.comprobanteasoccodigo) AS factura,",
+                     "c.comprobantedetalle as detallefactura",
+                     "FROM COMPROBANTES nota",
+                     "LEFT JOIN comprobantesasociados as asociados",
+                     "ON nota.tipocomprobantecodigo = asociados.tipocomprobantecodigo AND",
+                     "nota.comprobanteprefijo = asociados.comprobanteprefijo AND",
+                     "nota.comprobantecodigo = asociados.comprobantecodigo ",
+                     "LEFT JOIN comprobantes c",
+                     "ON asociados.comprobanteasoctipo = c.tipocomprobantecodigo AND",
+                     "asociados.comprobanteasocprefijo = c.comprobanteprefijo AND",
+                     "asociados.comprobantecodigo = c.comprobantecodigo",
+                     "WHERE nota.tipocomprobantecodigo IN ('NOTADB')",
+                     "AND asociados.comprobanteasoctipo IN ('FACA2','FACB2','FAECA','FAECB')")
+
+Facturas <- dbGetQuery(con,FacturasQuery)
+Facturas$detallefactura <- ifelse(is.na(Facturas$detallefactura),"SIN DETALLE",Facturas$detallefactura)
+
+Facturas$esRefactura <- str_detect(Facturas$detallefactura, "REFACTURA")
+
+MotivosDebitos <- left_join(MotivosDebitos,Facturas, by = c('notadb' = 'notadb'))
 
 MotivosDebitos <- select(MotivosDebitos,
                          "Fecha Emision Comprobante"= comprobantefechaemision,
@@ -84,12 +108,15 @@ MotivosDebitos <- select(MotivosDebitos,
                          "OOSS" = ooss,
                          "Efector" = efector,
                          "NroCRG" = crg,
+                         "Auditor" = auditor,
                          "Tipo Debito"= tipodebito,
                          "Categoria"= categoria,
                          "Observaciones" = observaciones,
                          "Rechazado" = rechazado,
                          "Aceptado" = aceptado,
-                         "Prestacion" = codigo)
+                         "Prestacion" = codigo,
+                         "AuditoriaCompartida" = EsAuditoriaCompartida,
+                         "EsRefactura" = esRefactura)
 
 lapply(dbListConnections(drv = dbDriver("PostgreSQL")), function(x) {dbDisconnect(conn = x)})
 
