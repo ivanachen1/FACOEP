1639ef2a7572ca7baa4cba07db24741e7df5770b
ivanachen1 <ivan.achenb@gmail.com>
2022-11-18 14:43:51 -0300

    logica de saldos preliminar

--- DBA/Reportes BI/2021/Matriz de Clientes/funciones_helper.py
+++ DBA/Reportes BI/2021/Matriz de Clientes/funciones_helper.py
@@ -42,3 +42,124 @@ class Tools():
                 print("PostgreSQL connection is closed")
         
         return(df)
+    
+    def get_text_query_comprobantes(tipoentidad,codCliente,sucursal,fecha,tiposcomprobantes,empcod):
+        
+        # Me falta encontrar el campo 
+        query_comprobantes =  ("SELECT "+ 
+                               "empcod,sucursalcodigo,comprobantetipoentidad,"+
+                               "comprobanteentidadcodigo, "+
+                               "tipocomprobantecodigo,comprobanteprefijo,comprobantecodigo,"+
+                               "comprobantefechaemision,comprobantetotalimporte FROM comprobantes "+
+                               "WHERE comprobantetipoentidad = {} AND "+
+                               "comprobanteentidadcodigo IN ({}) AND " +
+                               "sucursalcodigo = {} AND " +
+                               "comprobantefechaemision <= '{}' AND " +
+                               "tipocomprobantecodigo IN ({}) AND " +
+                               "empcod = '{}' AND " +
+                               "comprobanteasi IS NOT true AND " +
+                               "comprobanteasicuenta IS NULL").format(tipoentidad,codCliente,sucursal,
+                                                                      fecha,tiposcomprobantes,empcod)
+        
+        return(query_comprobantes)
+    
+    def get_tipo_comprobante_signo():
+        query = ("SELECT tipocomprobantecodigo,"+
+                "tipocomprobantesigno,tipocomprobantetipoasi "+
+                "FROM tipocomprobante WHERE sucursalcodigo = 2")
+                
+        return(query)
+    
+    def get_query_imputaciones(sucursalcodigo,sucursalcodigo2,tipoentidad,empcod,fechaemision,tipocomprobante):
+        
+        query = ("SELECT "+ 
+                 "imp.empcod, "+
+                 "imp.sucursalcodigo, "+
+                 "imp.comprobantetipoentidad, "+
+                 "imp.comprobanteentidadcodigo, "+
+                 "imp.tipocomprobantecodigo, "+
+                 "imp.comprobanteprefijo, "+
+                 "imp.comprobantecodigo, "+
+                 "imp.comprobanteimputaciontipo, "+
+                 "imp.comprobanteimputacionprefijo, "+
+                 "imp.comprobanteimputacioncodigo, "+
+                 "imp.comprobanteimputacionfecha, "+
+                 "imp.comprobanteimputacionimporte, "+
+                 "aux.comprobantefechaemision, "+
+                 "aux.comprobantetotalimporte, "+
+                 "tipo.tipocomprobantesigno, "+
+                 "tipo.tipocomprobantetipoasi "+
+
+                 "FROM comprobantesimputaciones as imp "+
+
+                 "LEFT JOIN comprobantes as aux "+
+                 "ON "+
+                 "imp.comprobanteimputacioncodigo = aux.comprobantecodigo AND "+
+                 "imp.comprobanteimputaciontipo = aux.tipocomprobantecodigo AND "+
+                 "imp.comprobanteimputacionprefijo = aux.comprobanteprefijo "+
+                
+                 "LEFT JOIN (SELECT tipocomprobantecodigo, "+
+                		   "tipocomprobantesigno, "+
+                		   "tipocomprobantetipoasi FROM tipocomprobante "+ 
+                		   "WHERE sucursalcodigo = {}) as tipo "+
+                 "ON aux.tipocomprobantecodigo = tipo.tipocomprobantecodigo "+
+                
+                 "WHERE imp.sucursalcodigo = {} AND imp.comprobantetipoentidad = {} AND "+
+                 "imp.empcod = '{}' AND aux.comprobantefechaemision <= '{}' AND "+
+                 "imp.tipocomprobantecodigo IN ({})").format(sucursalcodigo,
+                                                             sucursalcodigo2,
+                                                             tipoentidad,
+                                                             empcod,
+                                                             fechaemision,
+                                                             tipocomprobante)
+        return(query)                                                       
+    def verificar_fecha(tabla_comprobantes,tabla_imputaciones):
+        
+        total = []
+        for row in tabla_comprobantes.itertuples():
+            empcod = row.empcod
+            sucursalcodigo = row.sucursalcodigo
+            tipoentidad = row.comprobantetipoentidad
+            entidadcodigo = row.comprobanteentidadcodigo
+            tipocomprobante = row.tipocomprobantecodigo
+            comprobanteprefijo = row.comprobanteprefijo
+            comprobantecodigo = row.comprobantecodigo
+            valor = Tools.filter_imputaciones(tabla_imputaciones = tabla_imputaciones,
+                                              empcod = empcod,
+                                              sucursalcodigo = sucursalcodigo,
+                                              tipoentidad = tipoentidad,
+                                              entidadcodigo = entidadcodigo,
+                                              tipocomprobante = tipocomprobante,
+                                              comprobanteprefijo = comprobanteprefijo,
+                                              comprobantecodigo = comprobantecodigo)
+            total.append(valor)
+        
+        tabla_comprobantes['total_imputaciones'] = valor
+        
+        return(tabla_comprobantes)
+    
+    
+    
+    
+    
+    def filter_imputaciones(tabla_imputaciones,empcod,sucursalcodigo,
+                            tipoentidad,entidadcodigo,tipocomprobante,
+                            comprobanteprefijo,comprobantecodigo):
+        
+        
+        df = tabla_imputaciones.query("comprobanteentidadcodigo == @entidadcodigo and tipocomprobantecodigo.str.contains(@tipocomprobante) and comprobanteprefijo == @comprobanteprefijo and comprobantecodigo == @comprobantecodigo",engine = 'python')
+        
+        if len(df) == 0:
+            df['importe'] = 0
+        else:
+            df['comprobantetotalimporte'] = df['comprobantetotalimporte'].astype(float) 
+            df['importe'] = df['comprobantetotalimporte'] * df['tipocomprobantesigno']
+        
+        return(df)
+        
+        
+        
+        
+        
+        
+        
--- DBA/Reportes BI/2021/Matriz de Clientes/saldos_historicos.py
+++ DBA/Reportes BI/2021/Matriz de Clientes/saldos_historicos.py
@@ -15,6 +15,15 @@ query_clientes = 'SELECT clienteid FROM clientes'
 clientes = Tools.get_query(array_credentials = array_credentials,
                            query_text = query_clientes)
 
+clientes_list = clientes['clienteid'].values.tolist()
+
+lista_clientes = []
+for client in clientes_list:
+    var = "'"+str(client)+"'"
+    lista_clientes.append(var)
+    
+
+lista_clientes = ','.join(lista_clientes)
 
 query_tipo_comprobantes_validos = 'SELECT * FROM tipocomprobantelistados WHERE sucursalcodigo = 2 and tipocomprobantelistado = 3' 
 
@@ -23,21 +32,58 @@ tipos_validos = Tools.get_query(array_credentials = array_credentials,
 
 tipos_validos = tipos_validos['tipocomprobantecodigo'].unique()
 
-query_comprobantes =  ('SELECT'+ 
-                       'empcod,sucursalcodigo,comprobantetipoentidad,'+
-                       'tipocomprobantecodigo,comprobanteprefijo,comprobantecodigo'+
-                       'comprobantefechaemision,comprobantetotalimporte FROM comprobantes'+
-                       'WHERE comprobantetipoentidad = {comprobantetipoentidad} AND' +
-                       'sucursalcodigo = {sucursalcodigo} AND' +
-                       'comprobantefechaemision <= {comprobantefechaemision} AND' +
-                       'tipocomprobantecodigo IN {tipos_validos}' +
-                       'ttt')
+lista_tipos_validos = []
+for tipo in tipos_validos:
+    var = "'"+tipo+"'"
+    lista_tipos_validos.append(var)
+    
 
-#for cliente in clientes:
-#    comprobantetipoentidad = 2
-#    comprobantesaldo = 0
-#    sucursalcodigo = 2
-    
-#    empcod = 'ASI'
-    
+lista_tipos_validos = ','.join(lista_tipos_validos)
+#lista = '('+lista+')'
+
+
+text_comprobantes = Tools.get_text_query_comprobantes(tipoentidad = 2,
+                                                     codCliente = lista_clientes, 
+                                                     sucursal = 2, 
+                                                     fecha = '2022-07-31',
+                                                     tiposcomprobantes = lista_tipos_validos,
+                                                     empcod = 'ASI')
+
+comprobantes = Tools.get_query(array_credentials = array_credentials,
+                           query_text = text_comprobantes)
+
+
+
+text_imputaciones = Tools.get_query_imputaciones(sucursalcodigo = 2,
+                                                 sucursalcodigo2 =2,
+                                                 tipoentidad = 2,
+                                                 empcod = 'ASI',
+                                                 fechaemision = '2022-07-31',
+                                                 tipocomprobante = lista_tipos_validos)
+
+imputaciones = Tools.get_query(array_credentials = array_credentials,
+                               query_text = text_imputaciones)
+
+comprobantes['tipocomprobantecodigo'] = comprobantes['tipocomprobantecodigo'].str.strip()
+comprobantes['empcod'] = comprobantes['empcod'].str.strip()
+imputaciones['tipocomprobantecodigo'] = imputaciones['tipocomprobantecodigo'].str.strip()
+imputaciones['empcod'] = imputaciones['empcod'].str.strip() 
+
+
+#test = Tools.verificar_fecha(tabla_comprobantes = comprobantes,
+#                             tabla_imputaciones = imputaciones)
+
+testtt = Tools.filter_imputaciones(tabla_imputaciones = imputaciones,
+                                   empcod = 'ASI',
+                                   sucursalcodigo = 2,
+                                   tipoentidad = 2,
+                                   entidadcodigo = 461,
+                                   tipocomprobante = 'RECX2',
+                                   comprobanteprefijo = 1,
+                                   comprobantecodigo = 156) 
+
+imputaciones.dtypes    
+
+#imputaciones.to_csv('imputaciones.csv')   
+   
     
